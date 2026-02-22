8344da3d6bc248a3f513d2d3fe5183f49f3be581
Brage Sekse Aarset <brage@bjerk.io>
2026-02-22 11:47:57 +0200

    feat: functional client side link and image insertion (#2)
    * feat: establish client side link insertion
    
    * it works pretty well!
    
    * make image slightly bigger
    
    * feat: prepare for 1.0.0 release
    
    * fix: it works!
    
    * ignore temp folder

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,33 +1,54 @@
 'use strict';
-
-/**
- * This file shows how client-side javascript can be included via a plugin.
- * If you check `plugin.json`, you'll see that this file is listed under "scripts".
- * That array tells NodeBB which files to bundle into the minified javascript
- * that is served to the end user.
- *
- * There are two (standard) ways to wait for when NodeBB is ready.
- * This one below executes when NodeBB reports it is ready...
- */
-
 (async () => {
 	const hooks = await app.require('hooks');
 
-	hooks.on('action:app.load', () => {
-		// called once when nbb has loaded
-		console.log('hello fab!');
-	});
+	hooks.on('action:ajaxify.end', async (/* data */) => {
+		const response = await fetch('/plugins/nodebb-plugin-fab-cards/cards/cards.json');
+		const fabCardIndex = await response.json();
 
-	hooks.on('action:ajaxify.end', (/* data */) => {
-		// called everytime user navigates between pages including first load
+		// Build a map of display names to URL and image
+		const cardNameToData = {};
+		Object.values(fabCardIndex.cards).forEach(card => {
+			if (card.name && card.url && card.image) {
+				cardNameToData[card.name] = {
+					url: `https://cards.fabtcg.com${card.url}`,
+					image: card.image,
+				};
+			}
+		});
+
+		// Sort card names by length descending to avoid partial matches
+		const sortedCardNames = Object.keys(cardNameToData).sort((a, b) => b.length - a.length);
+
+		function insertCardLinks(html) {
+			sortedCardNames.forEach((cardName) => {
+				// Use word boundaries and allow for punctuation after the card name
+				const cardData = cardNameToData[cardName];
+				const regexEscapedCardName = cardName.replace(
+					/[-\/\\^$*+?.()|[\]{}]/g,
+					'\\$&',
+				);
+				const expression = new RegExp(
+					`(?<!<a[^>]*[^>]*>[^>]*|")${regexEscapedCardName}(?!["])`,
+					'g',
+				);
+				html = html.replaceAll(
+					expression,
+					`<a href="${cardData.url}" class="fab-card" target="_blank">
+					${cardName}<img src="${cardData.image}" alt="${cardName}">
+					</a>`,
+				);
+			});
+
+			
+			return html;
+		}
+		// find each element with the attribute component="post/content"
+		// and insert card links
+		$('[component="post/content"]').each((i, el) => {
+			const html = $(el).html();
+			const replaced = insertCardLinks(html);
+			$(el).html(replaced);
+		});
 	});
 })();
-
-/**
- * ... and this one reports when the DOM is loaded (but NodeBB might not be fully ready yet).
- * For most cases, you'll want the one above.
- */
-
-$(document).ready(function () {
-	// ...
-});
